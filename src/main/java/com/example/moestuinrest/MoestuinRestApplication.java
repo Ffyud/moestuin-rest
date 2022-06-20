5a623cba4a3cc43eb23c4869ae05a9fcdfbe9223
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-20 13:40:12 +0200

    commit all first

---
+++ src/main/java/com/example/moestuinrest/MoestuinRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.moestuinrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoestuinRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoestuinRestApplication.class, args);
+	}
+
+}
