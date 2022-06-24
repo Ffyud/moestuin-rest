9bccdbd2e887791b187c03b66df71d699049e460
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-24 12:40:21 +0200

    add query get by gardenId

--- src/main/java/com/example/moestuinrest/service/GardenContentService.java
+++ src/main/java/com/example/moestuinrest/service/GardenContentService.java
@@ -1,11 +1,16 @@
 package com.example.moestuinrest.service;
 
-import com.example.moestuinrest.model.Garden;
+import com.example.moestuinrest.model.GardenContent;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface GardenContentService extends CrudRepository<Garden, Integer> {
+public interface GardenContentService extends CrudRepository<GardenContent, Integer> {
 
+
+    @Query("select u from GardenContent u where u.garden.id = :gardenId")
+    Iterable<GardenContent> findAllByGardenId(@Param("gardenId") int gardenId);
 
 }
