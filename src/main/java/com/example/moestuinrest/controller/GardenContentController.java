f69e7a8989776b5d1c37829a90411be2e6915f94
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-06-28 16:24:25 +0200

    add delete and get for located gardencontent

--- src/main/java/com/example/moestuinrest/controller/GardenContentController.java
+++ src/main/java/com/example/moestuinrest/controller/GardenContentController.java
@@ -24,6 +24,18 @@ public class GardenContentController {
         return gardenContentService.findById(id);
     }
 
+//    TODO add distinct gardencontent
+
+    @GetMapping("/gardencontent/garden/{id}/{locationX}/{locationY}")
+    Optional<GardenContent> findByGardenIdAndLocation(@PathVariable int id, @PathVariable int locationX, @PathVariable int locationY) {
+        return gardenContentService.findByGardenIdAndLocation(id, locationX, locationY);
+    }
+
+    @DeleteMapping("/gardencontent/garden/{id}/{locationX}/{locationY}")
+    void deleteByGardenIdAndLocation(@PathVariable int id, @PathVariable int locationX, @PathVariable int locationY) {
+        gardenContentService.deleteByGardenIdAndLocation(id, locationX, locationY);
+    }
+
     @GetMapping("/gardencontent/garden/{id}")
     Iterable<GardenContent> findByGardenId(@PathVariable int id) { return gardenContentService.findAllByGardenId(id); }
 
--- src/main/java/com/example/moestuinrest/service/GardenContentService.java
+++ src/main/java/com/example/moestuinrest/service/GardenContentService.java
@@ -1,16 +1,30 @@
 package com.example.moestuinrest.service;
 
 import com.example.moestuinrest.model.GardenContent;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public interface GardenContentService extends CrudRepository<GardenContent, Integer> {
 
 
-    @Query("select u from GardenContent u where u.garden.id = :gardenId")
+    @Query("SELECT u FROM GardenContent u WHERE u.garden.id = :gardenId")
     Iterable<GardenContent> findAllByGardenId(@Param("gardenId") int gardenId);
 
+    @Query("SELECT u FROM GardenContent u WHERE u.positionX = :locationX " +
+            "AND u.positionY = :locationY AND u.garden.id = :gardenId")
+    Optional<GardenContent> findByGardenIdAndLocation(@Param("gardenId") int gardenId,
+                                                      @Param("locationX") int locationX, @Param("locationY") int locationY);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM GardenContent u WHERE u.positionX = :locationX " +
+            "AND u.positionY = :locationY AND u.garden.id = :gardenId")
+    void deleteByGardenIdAndLocation(@Param("gardenId") int gardenId,
+                                                        @Param("locationX") int locationX, @Param("locationY") int locationY);
 }
