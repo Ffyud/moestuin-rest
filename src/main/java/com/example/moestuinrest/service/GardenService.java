23bed86a96e8d00c362da01177b91d364912d0eb
Ffyud <5263778+Ffyud@users.noreply.github.com>
2022-07-05 11:17:40 +0200

    add delete endpoints

--- src/main/java/com/example/moestuinrest/service/GardenService.java
+++ src/main/java/com/example/moestuinrest/service/GardenService.java
@@ -1,12 +1,20 @@
 package com.example.moestuinrest.service;
 
 import com.example.moestuinrest.model.Garden;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public interface GardenService extends CrudRepository<Garden, Integer> {
 
-
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Garden u WHERE u.id = :gardenId")
+    void deleteByGardenId(@Param("gardenId") int gardenId);
 }
--- src/main/java/com/example/moestuinrest/service/PlantService.java
+++ src/main/java/com/example/moestuinrest/service/PlantService.java
@@ -1,9 +1,18 @@
 package com.example.moestuinrest.service;
 
 import com.example.moestuinrest.model.Plant;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public interface PlantService extends CrudRepository<Plant, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Plant u WHERE u.id = :plantId")
+    void deleteByPlantId(@Param("plantId") int plantId);
 }
